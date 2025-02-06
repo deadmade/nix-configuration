d8f0e246e7162100d7f0e460a1ac54864ae89b92
deadmade <130925709+deadmade@users.noreply.github.com>
2025-02-06 19:25:47 +0100

    ff

--- hosts/deadTest/config.nix
+++ hosts/deadTest/config.nix
@@ -21,6 +21,12 @@
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+    # VirtualBox aktivieren
+  virtualisation.virtualbox.host.enable = true;
+
+  # Falls du die Extension Pack Features brauchst:
+  virtualisation.virtualbox.host.enableExtensionPack = true;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
@@ -51,8 +57,8 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  services.displayManager.sddm.enable = false;
+  services.desktopManager.plasma6.enable = false;
 
   # Configure keymap in X11
   services.xserver.xkb = {
