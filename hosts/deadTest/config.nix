286fdc906206c8a33b1dc364a773e2d8c73d97fd
deadmade <130925709+deadmade@users.noreply.github.com>
2025-02-06 20:40:13 +0100

    test

--- hosts/deadTest/config.nix
+++ hosts/deadTest/config.nix
@@ -21,12 +21,6 @@
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-    # VirtualBox aktivieren
-  virtualisation.virtualbox.host.enable = true;
-
-  # Falls du die Extension Pack Features brauchst:
-  virtualisation.virtualbox.host.enableExtensionPack = true;
-
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
