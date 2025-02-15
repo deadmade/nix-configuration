50315ad42b4d6f7b3a594c10e87757c0bfdaa406
deadmade <manuel.schuelein@proton.me>
2025-02-15 13:16:17 +0100

    changed stylix theme + kitty theme

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -5,25 +5,9 @@
 }: {
   stylix = {
     enable = true;
-    image = ../../wallpapers/desktop/AnimeDeath.jpg;
-    # base16Scheme = {
-    #   base00 = "232136";
-    #   base01 = "2a273f";
-    #   base02 = "393552";
-    #   base03 = "6e6a86";
-    #   base04 = "908caa";
-    #   base05 = "e0def4";
-    #   base06 = "e0def4";
-    #   base07 = "56526e";
-    #   base08 = "eb6f92";
-    #   base09 = "f6c177";
-    #   base0A = "ea9a97";
-    #   base0B = "3e8fb0";
-    #   base0C = "9ccfd8";
-    #   base0D = "c4a7e7";
-    #   base0E = "f6c177";
-    #   base0F = "56526e";
-    # };
+    image = ../../wallpapers/stylix/bloodMoon.jpg;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/caroline.yaml";
+
     polarity = "dark";
     opacity = {
       terminal = 0.8;
