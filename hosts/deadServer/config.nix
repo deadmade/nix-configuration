a8908b85d5a38e2caab8d50cfc54082ea5c8e149
deadmade <130925709+deadmade@users.noreply.github.com>
2025-03-10 21:04:36 +0100

    disabled xserver

--- hosts/deadServer/config.nix
+++ hosts/deadServer/config.nix
@@ -38,7 +38,7 @@
 
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
+  services.xserver.enable = false;
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = false;
