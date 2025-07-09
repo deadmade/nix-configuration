712c341bfad623084587e6fdfcd2193870cb2cfe
deadmade <manuel.schuelein@proton.me>
2025-07-09 11:31:49 +0200

    added fwupd

--- modules/nixos/desktop/packages.nix
+++ modules/nixos/desktop/packages.nix
@@ -30,6 +30,7 @@
   ];
 
   services.flatpak.enable = true;
+  services.fwupd.enable = true;
 
   services.gvfs.enable = true; # Mount, trash, and other functionalities
   services.tumbler.enable = true; # Thumbnail support for images
